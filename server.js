e92efed659fb2e27550b740d40d2a3a3a2c91d5d
Heber Cordova <u201924464@upc.edu.pe>
2022-06-26 23:10:55 -0500

    fix: fixed location of database

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+const data = require("server/db.json")
 const jsonServer = require("json-server");
 const jsonSeverAuth = require("json-server-auth");
 const app = jsonServer.create();
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(data);
 const port = process.env.PORT || 3000;
 
 app.db = router.db;
